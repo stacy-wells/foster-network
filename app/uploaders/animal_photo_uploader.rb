0fee5a2b591f3a91d680645a0ff4cfe8a4169d55
stacy-wells <slwells918@gmail.com>
2015-10-12 15:29:56 -0400

    hound

--- app/uploaders/animal_photo_uploader.rb
+++ app/uploaders/animal_photo_uploader.rb
@@ -9,7 +9,7 @@ class AnimalPhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url(*args)
+  def default_url(*)
     ActionController::Base.helpers.asset_path(
       "assets/images/fallback/" + [nil, "dog.png"].compact.join("_"))
   end
